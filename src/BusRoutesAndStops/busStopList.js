bbe70807555b8e496614241f390451086b56086b
cameronOBrien12 <cameronobrien12@hotmail.co.uk>
2018-09-13 17:19:42 +0100

    Cleaned up bus routes/ bus stops data from logic
    Bus Stops/Stations array is now located in seperate file.
    
    Minor changes to css of routes.

---
+++ src/BusRoutesAndStops/busStopList.js
@@ -0,0 +1,15 @@
+var locations = [
+      			[56.459659, -2.970661],
+      			[56.455913, -2.991500],
+      			[56.464413, -3.039369],
+      			[56.464913, -3.056310],
+      			[56.459983, -2.969878], //Crichton Street (C2) bus stop. 
+      			[56.478883, -2.952696], //Lamerton Terrace bus stop.
+      			[56.493383, -2.935541], // Findowrie Street terminus - (Fintry Road) bus stop
+    			];
+    			
+var locationBusStations = [
+      			[ 56.4631, -2.966], // Seagate
+    			];
+
+   
