417ef6c9c4d22b0a2642a4e85d689c8e6f152368
nestor <nestorsdelgado@gmail.com>
2018-09-14 11:36:33 +0100

    Added route 1 bus stops & new marker

--- src/busRoutes/busStopList.js
+++ src/busRoutes/busStopList.js
@@ -1,11 +1,24 @@
 var locations = [
-      			[56.459659, -2.970661],
-      			[56.455913, -2.991500],
-      			[56.464413, -3.039369],
-      			[56.464913, -3.056310],
+				
+				//Route 1
       			[56.459983, -2.969878], //Crichton Street (C2) bus stop. 
-      			[56.478883, -2.952696], //Lamerton Terrace bus stop.
-      			[56.493383, -2.935541], // Findowrie Street terminus - (Fintry Road) bus stop
+      			[56.478883, -2.952696], //Lamerton Terrace bus stop. 
+      			[56.493383, -2.935541], // Findowrie Street terminus - (Fintry Road) bus stop  
+      			[56.462073, -2.977424], //City Centre - Court house Square 
+      			[56.463148, -2.970305], //Albert Square 
+      			[56.471851, -2.968487], //Dens Road 
+      			[56.471290, -2.973604], //Hiltown - Main Street 
+      			[56.472479, -2.992712], //The Glens - Byron Crescent West 
+      			[56.473129, -3.005436], //Lochee - The Stack 
+      			[56.480609, -3.001139], //Kings Cross Road - opp B&Q 
+      			[56.483013, -3.011505], //Baird Avenue - Tesco Call Centre 
+      			[56.496252, -3.018334], //St Marys - Laird Street, Benholm Place 
+				
+				//Route 5
+      			[56.459659, -2.970661], //City Centre - Whitehall Street  
+      			[56.455913, -2.991500], //West-End Perth Road - Step-Row  
+      			[56.464413, -3.039369], //Ninewells Hospital - Stance B  
+      			[56.464913, -3.056310], //Technology Park - Luna Place  
     			];
     			
 var locationBusStations = [
