d9af00e448757d55257bd9b915bf480f277139a9
Rony Portelli <portelli.rony@gmail.com>
2018-09-14 12:45:59 +0100

    created tool to add bus routes

--- models/busses.js
+++ models/busses.js
@@ -2,19 +2,23 @@ const mongoose = require('mongoose');
 
 const busSchema = mongoose.Schema({
     //Number of the bus
-    number:{
+    number : {
       type : Number,
-      required: true
-    },
-    //Bus stops for a specific bus
-    stops:{
-      type : [{name: String, lat: Number,lon:Number}],
       required : true
     },
-    schedule:{
+    //Bus stops for a specific bus
+    stops : {
+      type : [[{name: String, lat: Number,lng:Number}]],
+      required : false
+    },
+    route : {
+      type : [],
+      required : false
+    },
+    schedule : {
         type : [{name:String,time:[]}],
-        require : true
+        require : false
     }
   });
 
-  const bus = module.exports = mongoose.model('Bus',busSchema);
+  const Bus = module.exports = mongoose.model('Bus',busSchema);
--- routes/busStops.js
+++ routes/busStops.js
@@ -3,6 +3,11 @@ const mongoose = require('mongoose');
 const router = express.Router();
 const path = require('path');
 let Bus = require('../models/busses');
+let busRoute = [];
+
+router.get('/createRoute',(req,res) => {
+  res.sendFile(path.join(__dirname + '/../src/mapAddRoutes.html'));
+});
 
 router.get('/getBusStops',(req,res) =>{
     console.log(req.param('busNumber'));
@@ -13,14 +18,28 @@ router.get('/getBusStops',(req,res) =>{
       }
     });
 });
+router.post('/createRoute',(req,res) =>{
+  console.log(req.body);
+  busRoute.push([JSON.parse(req.body.position).lat,JSON.parse(req.body.position).lng]);
+  console.log(busRoute);
+});
+router.post('/generateRoute',(req,res) =>{
+  console.log(busRoute);
+  Bus.findOneAndUpdate({number:req.param('busNumber')},{$push: {route:{$each: busRoute}}}, function (err){
+    if(err) throw err;
+    busRoute = [];
+    res.send('Route Added');
+  });
+});
 
-router.post('/addBusStop',(req,res) =>{
+router.get('/addBusStop',(req,res) =>{
     console.log(req.param('busNumber'));
-    Bus.find({number:req.param('busNumber')}, (err,stops) =>{
-      if(err) throw err;
-      if(stops){
-        res.json(stops);
-      }
+    var bus = new Bus({
+      number: req.param('busNumber')
+    });
+    bus.save(function (err) {
+      if (err) return handleError(err);
+      res.send('it works');
     });
 });
 
