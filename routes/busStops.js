38614064156d9ebc69e8d3579f99be3078bbaa1f
cangi <portelli.rony@gmail.com>
2018-09-17 21:54:22 +0100

    Refactored the code and added tests for backend calls

--- routes/busStops.js
+++ routes/busStops.js
@@ -10,56 +10,73 @@ router.get('/createRoute',(req,res) => {
 });
 
 router.get('/getBusStops',(req,res) =>{
-    console.log(req.param('busNumber'));
+  console.log(req.param('busNumber'));
+    if(req.param('busNumber') === undefined){
+      res.status(400);
+      res.send("Bus number not specified");
+    }else{
     Bus.find({number:req.param('busNumber')}, (err,stops) =>{
       if(err) throw err;
       if(stops){
+        res.status(200);
         res.json(stops);
       }
     });
+  }
 });
 router.post('/createRoute',(req,res) =>{
-  console.log(req.body);
   busRoute.push([JSON.parse(req.body.position).lat,JSON.parse(req.body.position).lng]);
-  console.log(busRoute);
 });
 router.post('/generateRoute',(req,res) =>{
-  console.log(busRoute);
-  Bus.findOne({number:req.param('busNumber')}, function (err,bus){
-    if(err) throw err;
-    if(bus){
-      Bus.findOneAndUpdate({number:req.param('busNumber')}, {$set:{route: busRoute}}, (err) => {
-        if(err) throw err;
-     })
-    }else{
-      var bus = new Bus({
-        number: req.param('busNumber'),
-        route : busRoute
-      });
-      bus.save(function (err) {
-        if (err) return handleError(err);
-      });
-      console.log("new one added");
-    }
-    busRoute = [];
-    res.send('Route Added');
-  });
+  if(req.param('busNumber') === undefined){
+    res.status(400);
+    res.send("Bus number not specified")
+  }else{
+    Bus.findOne({number:req.param('busNumber')}, function (err,bus){
+      if(err) throw err;
+      if(bus){
+        Bus.findOneAndUpdate({number:req.param('busNumber')}, {$set:{route: busRoute}}, (err) => {
+          if(err){
+            throw err;
+          }
+         })
+      }else{
+        var bus = new Bus({
+          number: req.param('busNumber'),
+          route : busRoute
+        });
+        bus.save(function (err) {
+          if (err){
+            return handleError(err);
+          }
+        });
+      }
+      busRoute = [];
+      res.status(200)
+      res.send('Route Added');
+    });
+  }
 });
 
 router.get('/resetRoute', (req,res) => {
   busRoute = [];
-  res.send("reseted");
+  res.send("Reseted");
 })
 
 router.get('/addBusStop',(req,res) =>{
-    console.log(req.param('busNumber'));
-    var bus = new Bus({
-      number: req.param('busNumber')
-    });
-    bus.save(function (err) {
-      if (err) return handleError(err);
-      res.send('it works');
-    });
+    if(req.param('busNumber') === undefined){
+      res.status(400);
+      res.send("Bus number not specified")
+    }else{
+      var bus = new Bus({
+        number: req.param('busNumber')
+      });
+      bus.save(function (err) {
+        if (err) return handleError(err);
+        res.status(200);
+        res.send('Bus added');
+      });
+    }
 });
 
 module.exports = router;
