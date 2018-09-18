9b3bc03cf873bc8acc7fd0e35755180d81c5c308
nestor <nestorsdelgado@gmail.com>
2018-09-18 12:35:20 +0100

    Bus Stops
    Bus stop windows now show nº and route

--- src/busRoutes/busStopList.js
+++ src/busRoutes/busStopList.js
@@ -1,34 +1,37 @@
+
 var locations = [
 				
+				//X coordinate, Y coordinate, bus stop nº, bus route nº
+				
 				//Route 1
-      			[56.459983, -2.969878, 0], //Crichton Street (C2) bus stop. 
-      			[56.478883, -2.952696, 1], //Lamerton Terrace bus stop. 
-      			[56.493383, -2.935541, 2], // Findowrie Street terminus - (Fintry Road) bus stop  
-      			[56.462073, -2.977424, 3], //City Centre - Court house Square 
-      			[56.463148, -2.970305, 4], //Albert Square 
-      			[56.471851, -2.968487, 5], //Dens Road 
-      			[56.471290, -2.973604, 6], //Hiltown - Main Street 
-      			[56.472479, -2.992712, 7], //The Glens - Byron Crescent West 
-      			[56.473129, -3.005436, 8], //Lochee - The Stack 
-      			[56.480609, -3.001139, 9], //Kings Cross Road - opp B&Q 
-      			[56.483013, -3.011505, 10], //Baird Avenue - Tesco Call Centre 
-      			[56.496252, -3.018334, 11], //St Marys - Laird Street, Benholm Place 
+      			[56.459983, -2.969878, 0, 1], //Crichton Street (C2) bus stop. 
+      			[56.478883, -2.952696, 1, 1], //Lamerton Terrace bus stop. 
+      			[56.493383, -2.935541, 2, 1], // Findowrie Street terminus - (Fintry Road) bus stop  
+      			[56.462073, -2.977424, 3, 1], //City Centre - Court house Square 
+      			[56.463148, -2.970305, 4, 1], //Albert Square 
+      			[56.471851, -2.968487, 5, 1], //Dens Road 
+      			[56.471290, -2.973604, 6, 1], //Hiltown - Main Street 
+      			[56.472479, -2.992712, 7, 1], //The Glens - Byron Crescent West 
+      			[56.473129, -3.005436, 8, 1], //Lochee - The Stack 
+      			[56.480609, -3.001139, 9, 1], //Kings Cross Road - opp B&Q 
+      			[56.483013, -3.011505, 10, 1], //Baird Avenue - Tesco Call Centre 
+      			[56.496252, -3.018334, 11, 1], //St Marys - Laird Street, Benholm Place 
 				
 				//Route 5
-      			[56.459659, -2.970661, 12], //City Centre - Whitehall Street  
-      			[56.455913, -2.991500, 13], //West-End Perth Road - Step-Row  
-      			[56.464413, -3.039369, 14], //Ninewells Hospital - Stance B  
-      			[56.464913, -3.056310, 15], //Technology Park - Luna Place  
+      			[56.459659, -2.970661, 12, 5], //City Centre - Whitehall Street  
+      			[56.455913, -2.991500, 13, 5], //West-End Perth Road - Step-Row  
+      			[56.464413, -3.039369, 14, 5], //Ninewells Hospital - Stance B  
+      			[56.464913, -3.056310, 15, 5], //Technology Park - Luna Place  
 				
 				//Route 23S
-				 [56.480319, -3.022696, 16],//Linton Road BUS STOP
-				 [56.471722, -3.016213, 17],//Sharps Court BUS STOP 
-				 [56.464778, -3.026638, 18],//The Shand BUS STOP
-				 [56.456497, -3.009936, 19] //Harris Academy Perth Road BUS STOP.
+				 [56.480319, -3.022696, 16, "23S"],//Linton Road BUS STOP
+				 [56.471722, -3.016213, 17, "23S"],//Sharps Court BUS STOP 
+				 [56.464778, -3.026638, 18, "23S"],//The Shand BUS STOP
+				 [56.456497, -3.009936, 19, "23S"] //Harris Academy Perth Road BUS STOP.
     			];
     			
 var locationBusStations = [
-      			[ 56.4631, -2.966], // Seagate
+      			[ 56.4631, -2.966, 0], // Seagate
     			];
 
    
