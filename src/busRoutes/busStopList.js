475f475362ff8cbd38aa9f02caf28791a996afcf
nestor <nestorsdelgado@gmail.com>
2018-09-18 13:31:33 +0100

    Fixed bus stop lanes

--- src/busRoutes/busStopList.js
+++ src/busRoutes/busStopList.js
@@ -1,47 +1,57 @@
 
 var locations = [
 				
-				//X coordinate, Y coordinate, bus stop nº, bus route nº
+				//X coordinate, Y coordinate, bus stop nº, bus route nº, name of the bus stop/station
 				
 				//Route 1
-      			[56.459983, -2.969878, 1, 1], //Crichton Street (C2) bus stop. 
-      			[56.478883, -2.952696, 2, 1], //Lamerton Terrace bus stop. 
-      			[56.493383, -2.935541, 3, 1], // Findowrie Street terminus - (Fintry Road) bus stop  
-      			[56.462073, -2.977424, 4, 1], //City Centre - Court house Square 
-      			[56.463148, -2.970305, 5, 1], //Albert Square 
-      			[56.471851, -2.968487, 6, 1], //Dens Road 
-      			[56.471290, -2.973604, 7, 1], //Hiltown - Main Street 
-      			[56.472479, -2.992712, 8, 1], //The Glens - Byron Crescent West 
-      			[56.473129, -3.005436, 9, 1], //Lochee - The Stack 
-      			[56.480609, -3.001139, 10, 1], //Kings Cross Road - opp B&Q 
-      			[56.483013, -3.011505, 11, 1], //Baird Avenue - Tesco Call Centre 
-      			[56.496252, -3.018334, 12, 1], //St Marys - Laird Street, Benholm Place 
+	 			[56.462073, -2.977424, 1, 1, "Court House Square"],
+	 			[56.463148, -2.970305, 2, 1, "Albert Square"],
+	 			[56.471851, -2.968487, 3, 1, "Dens Road"],
+	 			[56.471290, -2.973604, 4, 1, "Hiltown"],
+	 			[56.472479, -2.992712, 5, 1, "The Glens - Byron Crescent West"],
+	 			[56.473129, -3.005436, 6, 1, "Lochee - The Stack"],
+	 			[56.480609, -3.001139, 7, 1, "Kings Cross Road - opp B&Q"],
+	 			[56.483013, -3.011505, 8, 1, "Baird Avenue - Tesco Call Centre"],
+	 			[56.496252, -3.018334, 9, 1, "St Marys - Laird Street, Benholm Place"],
+	 			
+	 			//Route 32
+	 			[56.459983, -2.969878, 10, 32, "Crichton Street"],
+	 			[56.478869, -2.952626, 11, 32, "Lamerton Terrace"],
+	 			[56.493258, -2.935696, 12, 32, "Findowrie Street"],
 				
 				//Route 5
-      			[56.459659, -2.970661, 13, 5], //City Centre - Whitehall Street  
-      			[56.455913, -2.991500, 14, 5], //West-End Perth Road - Step-Row  
-      			[56.464413, -3.039369, 15, 5], //Ninewells Hospital - Stance B  
-      			[56.464913, -3.056310, 16, 5], //Technology Park - Luna Place  
+      			[56.459659, -2.970661, 13, 5, "Whitehall Street "],  
+      			[56.455913, -2.991500, 14, 5, "West-End Perth Road - Step-Row"],  
+      			[56.464413, -3.039369, 15, 5, "Ninewells Hospital - Stance B"],  
+      			[56.464913, -3.056310, 16, 5, "Technology Park - Luna Place"],  
 				
 				//Route 23S
-				 [56.480319, -3.022696, 17, "23S"],//Linton Road BUS STOP
-				 [56.471722, -3.016213, 18, "23S"],//Sharps Court BUS STOP 
-				 [56.464778, -3.026638, 19, "23S"],//The Shand BUS STOP
-				 [56.456497, -3.009936, 20, "23S"], //Harris Academy Perth Road BUS STOP.
+				 [56.480319, -3.022696, 17, "23S", "Linton Road"],
+				 [56.471722, -3.016213, 18, "23S", "Sharps Court"],
+				 [56.464778, -3.026638, 19, "23S", "The Shand"],
+				 [56.456497, -3.009936, 20, "23S", "Harris Academy - Perth Road"],
     		
 				//Route 75
-				[56.459985, -2.970808, 21, 75],//City Centre, High Street (Stop 3)
-				//[56.463849, -2.964979, 21, 75],//City Centre, Bus Station
-				[56.469494, -2.894323, 22, 75],//West Ferry/Broughty Ferry, Ogilvie Court
-				[56.468344, -2.875823, 23, 75],//Broughty Ferry, Library
-				[56.481033, -2.836319, 24, 75],//Monifieth, Grange Primary School
-				[56.490562, -2.837750, 26, 75]//Ethiebeaton Park, Dobbie's Garden Centre
+				[56.459985, -2.970808, 21, 75, "High Street"],
+				//[56.463849, -2.964979, 21, 75], //City Centre, Bus Station
+				[56.469494, -2.894323, 22, 75, "West Ferry/Broughty Ferry, Ogilvie Court"],
+				[56.468344, -2.875823, 23, 75, "Broughty Ferry, Library"],
+				[56.481033, -2.836319, 24, 75, "Monifieth, Grange Primary School"],
+				[56.490562, -2.837750, 25, 75, "Ethiebeaton Park, Dobbie's Garden Centre"],
+				
+				//Route 4
+				[56.459433, -2.970198, 26, 4, "Whitehall Street W1"],
+				[56.462957, -2.999958, 27, 4, "Logie Estate - Scott Street, Ashbank Road"],
+				[56.469602, -3.000405, 28, 4, "Kilberry Street - Fullerton Street"],
+				[56.473345, -3.011184, 29, 4, "Lochee Bank Street"],
+				[56.477685, -3.017801, 30, 4, "Park Hotel"],
+				[56.480307, -3.022678, 31, 4, "Dryburgh - Linton Road"],	
 				];
 				
 
 var locationBusStations = [
       			
-      			[56.463849, -2.964979, 1, 75],//City Centre, Bus Station
+      			[56.463849, -2.964979, 1, 75, "City Centre Bus Station"],
     			];
 
    
